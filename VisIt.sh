2a3c7ec223b1781d0a72eedfa991ab89d91216ce
Caleb Munger <mungerct@iastate.edu>
2025-12-18 13:54:55 -0600

    updated nova script

--- VisIt.sh
+++ VisIt.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
-#SBATCH --job-name=visit_job          # Job name
+#!/usr/bin/env bash
+#SBATCH --job-name="VisIt"            # Job name
 #SBATCH --output=visit_job.%j.out     # Standard output log
-#SBATCH --error=visit_job.%j.err      # Standard error log
-#SBATCH --ntasks=32                     # Number of tasks (MPI processes)
-#SBATCH --cpus-per-task=1             # Number of CPU cores per task
-#SBATCH --time=1:00:00               # Max walltime (hh:mm:ss)
-#SBATCH --partition=compute           # Partition/queue name
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=36
+#SBATCH --time=1:00:00                # Max walltime (hh:mm:ss)
+#SBATCH --mail-user=mungerct@iastate.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
 
 # Load any necessary modules
-module load visit/3.1.4               
-
-# Navigate to your working directory
-cd /home/mungerct/research/alamo/
+module load visit/3.4.1-py311-openmpi4-gns5n4x
 
 # Run Visit in parallel
-visit -cli -nowin -np 32 -s high_temp_all_time.py /home/mungerct/research/alamo/output.scpthermalIAstate.old.nova_small_flame_eps/
+visit -cli -nowin -np 36 -s high_temp_all_time.py /home/brunnels/mungerct/alamo/output.scpthermalIAstate.old.nova_small_flame_eps/
